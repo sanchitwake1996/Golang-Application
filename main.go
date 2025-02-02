0bfb4a106eb83e94e23eaacc148596cf3a291f2a
sanchitwake1996 <sanchitwake1997@gmail.com>
2025-02-02 13:26:31 +0530

    Update main.go

--- main.go
+++ main.go
@@ -12,5 +12,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Starting server on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe("0.0.0.0:8080", nil)
+
 }
