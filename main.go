317cf61b37ba4322af9641ff70953410190acd91
sanchitwake1996 <sanchitwake1997@gmail.com>
2025-02-02 14:12:56 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from AWS Code Pipeline using Go by developer sanchit wake!")
+	fmt.Fprintf(w, "Hello from AWS Code Pipeline using Go by developer sanchit jaywant wake!")
 }
 
 func main() {
