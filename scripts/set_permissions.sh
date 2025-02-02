8e973b37c08c6def82c8dbe165cfbd33732fc2d3
sanchitwake1996 <sanchitwake1997@gmail.com>
2025-02-02 12:13:59 +0530

    Update set_permissions.sh

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -2,7 +2,11 @@
 set -e
 set -x
 
+echo "Fixing ownership of the Go application..."
+sudo chown ubuntu:ubuntu /home/ubuntu/myapp/myapp || { echo "Failed to change ownership"; exit 1; }
+
 echo "Setting permissions for the Go application..."
-chmod +x /home/ubuntu/myapp/myapp || { echo "Failed to set executable permissions"; exit 1; }
+sudo chmod +x /home/ubuntu/myapp/myapp || { echo "Failed to set executable permissions"; exit 1; }
 
 echo "Permissions set successfully!"
+
