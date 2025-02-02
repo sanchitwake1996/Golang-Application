b07419f9fb580aefe2fe8b745e4894e9ae6aff16
sanchitwake1996 <sanchitwake1997@gmail.com>
2025-02-02 11:59:41 +0530

    Delete scripts/clean_old_files.sh

--- scripts/clean_old_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-echo "Cleaning up old files..."
-rm -rf /home/ubuntu/myapp/* || { echo "Failed to clean old files"; exit 1; }
-
-echo "Old files removed successfully!"
